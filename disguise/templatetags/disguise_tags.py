4bb6a1e07eb48ee7d3650287682182f2dc97c17b
Mikhail Porokhovnichenko <marazmiki@gmail.com>
2020-04-09 04:21:40 +0700

    Reworked templates a bit (get rid jQuery off, added some inheritance to let a user build his awesome UI

--- disguise/templatetags/disguise_tags.py
+++ disguise/templatetags/disguise_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from disguise.forms import DisguiseForm
+from disguise.forms import get_disguise_form_class
 from disguise.utils import can_disguise
 
 register = template.Library()
@@ -16,7 +16,7 @@ def disguise_widget(context):
     request = context['request']
     context.update({
         'can_disguise': can_disguise(request),
-        'form': DisguiseForm(),
+        'form': get_disguise_form_class()(),
         'original_user': getattr(request, 'original_user', None),
         'disguise_user': request.user,
     })
